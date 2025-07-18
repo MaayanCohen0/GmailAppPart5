324902668e0f62560cfbffe2eea19afba4b494b0
TamarRoseBIU <tamar.rosenzweig@live.biu.ac.il>
2025-07-18 13:35:22 +0300

    ASP-338 nav fix

--- src/AndroidApp/MyEmailApp/app/src/main/java/com/example/myemailapp/MainActivity.java
+++ src/AndroidApp/MyEmailApp/app/src/main/java/com/example/myemailapp/MainActivity.java
@@ -329,7 +329,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             fragmentTag = "profile";
         }
 
-        if (fragment != null && !fragmentTag.equals(currentFragment)) {
+        if (fragment != null) {
             loadFragment(fragment, title);
             currentFragment = fragmentTag;
         }
