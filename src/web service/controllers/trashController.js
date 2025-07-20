0d038c3c4bf9ff13fc3f35edbbaf74ac59805a7e
Eden Atias <edena2003@gmail.com>
2025-07-21 01:02:50 +0300

    fixes in spam and trash

--- src/web service/controllers/trashController.js
+++ src/web service/controllers/trashController.js
@@ -2,7 +2,7 @@ const trashService = require("../services/trashService");
 const { getUserById } = require("../services/userService");
 const mailService = require("../services/mailService");
 const draftService = require("../services/draftService");
-const spamModel = require("../models/spamModel");
+const spamService = require("../services/spamService");
 
 // Get all trash mails of the user
 exports.getTrashMails = async (req, res) => {
@@ -111,11 +111,11 @@ exports.restoreTrashMail = async (req, res) => {
       destination = "inbox";
       break;
     case "drafts":
-      copiedMail = draftService.createCopyOfDraft(mail, username);
+      copiedMail = await draftService.createCopyOfDraft(mail, username);
       destination = "drafts";
       break;
     case "spam":
-      copiedMail = spamModel.createCopyOfSpam(mail, username);
+      copiedMail = await spamService.createCopyOfSpam(mail, username);
       destination = "spam";
       break;
     default:
@@ -257,14 +257,14 @@ exports.addToTrashFromSpam = async (req, res) => {
   }
 
   const username = user.username;
-  const mail = spamModel.getSpamById(id, username);
+  const mail = await spamService.getSpamById(id, username);
 
   if (!mail) {
     return res.status(404).json({ error: "Spam not found for this user" });
   }
 
   try {
-    const result = spamModel.finalDeleteSpamById(id, username);
+    const result = await spamService.finalDeleteSpamById(id, username);
     if (!result) {
       return res.status(500).json({ error: "Failed to delete mail from spam" });
     }
--- src/web service/models/spamModel.js
+++ src/web service/models/spamModel.js
@@ -9,11 +9,11 @@ const spamSchema = new mongoose.Schema({
   body:       { type: String, default: "" },
   labels:     { type: [String], default: [] },
   timeStamp:  { type: Date, default: Date.now },
-  mailType:   { type: String, enum: ["sent", "received", "sent and received"], default: "received" },
+  mailType:   { type: String, enum: ["sent", "received", "sent and received"], default: "sent" },
   owner:      { type: String, required: true, index: true },
   isRead:     { type: Boolean, default: false },
   isStarred:  { type: Boolean, default: false },
-  source:     { type: String, enum: ["inbox", "drafts", "spam"], default: "spam" },
+  source:     { type: String, enum: ["inbox", "drafts", "spam", "trash"], default: "inbox" },
 });
 
 spamSchema.index({ id: 1, owner: 1 }, { unique: true });
