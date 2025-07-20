27312c302231fba61d6e673df5e3a7352cf23211
MaayanCohen0 <maayan.c100@gmail.com>
2025-07-20 03:17:33 +0300

    mails and drafts updating duo to labels

--- src/web service/services/labelsService.js
+++ src/web service/services/labelsService.js
@@ -1,4 +1,8 @@
 const Label = require("../models/labelsModel");
+const Mail = require("../models/mailsModel");
+const Draft = require("../models/draftsModel");
+//const Spam = require("../models/spamModel");
+//const Trash = require("../models/trashModel");
 
 const getLabels = async (userId, limit = null) => {
   const query = Label.find({ userId });
@@ -22,10 +26,88 @@ const addLabel = async (name, userId) => {
   return await newLabel.save();
 };
 
-const updateLabel = async (id, name, userId) => {
+
+async function updateLabelInAllElements(oldName, newName, username) {
+  try {
+
+    const updateQuery = {
+      $set: { "labels.$[elem]": newName },
+    };
+    const options = {
+      arrayFilters: [{ elem: oldName }],
+    };
+
+    const mailsUpdate = await Mail.updateMany(
+      { owner: username, labels: oldName },
+      updateQuery,
+      options
+    );
+
+    const draftsUpdate = await Draft.updateMany(
+      { owner: username, labels: oldName },
+      updateQuery,
+      options
+    );
+/*
+    const spamUpdate = await Spam.updateMany(
+      { owner: username, labels: oldName },
+      updateQuery,
+      options
+    );
+
+    const trashUpdate = await Trash.updateMany(
+      { owner: username, labels: oldName },
+      updateQuery,
+      options
+    );
+*/
+    //return { mailsUpdate, draftsUpdate, spamUpdate, trashUpdate };
+    return { mailsUpdate, draftsUpdate };
+  } catch (err) {
+    console.error("Error updating label in all elements:", err);
+    return {};
+  }
+}
+
+async function deleteLabelInAllElements(nameToRemove, username) {
+  try {
+    const mailsUpdate = await Mail.updateMany(
+      { owner: username, labels: nameToRemove },
+      { $pull: { labels: nameToRemove } }
+    );
+
+    const draftsUpdate = await Draft.updateMany(
+      { owner: username, labels: nameToRemove },
+      { $pull: { labels: nameToRemove } }
+    );
+/*
+    const spamUpdate = await Spam.updateMany(
+      { owner: username, labels: nameToRemove },
+      { $pull: { labels: nameToRemove } }
+    );
+
+    const trashUpdate = await Trash.updateMany(
+      { owner: username, labels: nameToRemove },
+      { $pull: { labels: nameToRemove } }
+    );
+*/
+    //return { mailsUpdate, draftsUpdate, spamUpdate, trashUpdate };
+    return { mailsUpdate, draftsUpdate };
+  } catch (err) {
+    console.error("Error deleting label in all elements:", err);
+    return {};
+  }
+}
+
+
+
+
+const updateLabel = async (id, name, userId, username) => {
   const label = await Label.findById(id).exec();
   if (!label) throw new Error("Label not found");
   if (label.userId !== userId) throw new Error("Unauthorized");
+
+
 
   const conflict = await Label.findOne({
     _id: { $ne: id },
@@ -34,16 +116,25 @@ const updateLabel = async (id, name, userId) => {
   });
   if (conflict) throw new Error("Label with this name already exists");
 
+  const oldName = label.name;
   label.name = name;
-  return await label.save();
+  const savedLabel = await label.save();
+
+  await updateLabelInAllElements(oldName, name, username);
+
+  return savedLabel;
 };
 
-const deleteLabel = async (id, userId) => {
+const deleteLabel = async (id, userId, username) => {
   const label = await Label.findById(id).exec();
   if (!label) throw new Error("Label not found");
   if (label.userId !== userId) throw new Error("Unauthorized");
+  const oldName = label.name;
 
   await label.deleteOne();
+
+  await deleteLabelInAllElements(oldName, username);
+
   return true;
 };
 
